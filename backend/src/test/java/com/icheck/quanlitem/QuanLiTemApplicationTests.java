181f6d8e2b9b960d8fe64edafc89f50ddace51f5
taQuangLing <linh.tq194605@gmail.com>
2022-09-22 15:54:22 +0700

    create backend

---
+++ backend/src/test/java/com/icheck/quanlitem/QuanLiTemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icheck.quanlitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLiTemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
